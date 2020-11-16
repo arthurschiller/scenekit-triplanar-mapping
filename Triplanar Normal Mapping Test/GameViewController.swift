b6f175a2ca696a0861d018ded303ae470fabf63f
Arthur Schiller <mail@arthurschiller.de>
2020-11-16 21:31:49 +0100

    - initial commit

---
+++ Triplanar Normal Mapping Test/GameViewController.swift
@@ -0,0 +1,52 @@
+//
+//  GameViewController.swift
+//  Triplanar Normal Mapping Test
+//
+//  Created by Arthur Schiller on 16.11.20.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+        let scene = SCNScene(named: "art.scnassets/triplanarMappingScene.scn")!
+        
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.black
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+}
